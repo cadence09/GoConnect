be14f79bfc9252c5e1402078f65c9a065c68a94a
Caden <cadence1547@gmail.com>
2020-04-15 15:16:33 -0700

    save change

--- chat/src/screens/camera.js
+++ chat/src/screens/camera.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Camera } from 'expo-camera';
 import {
-  View, TouchableOpacity, Text, StyleSheet,
+  View, TouchableOpacity, Text, StyleSheet,Alert
 } from 'react-native';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function TakePhoto() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [cameraInvo, setCameraInvo] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [cameraRoll, setCameraRoll]=useState(false)
+
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      let { status } = await Permissions.askAsync(Permissions.CAMERA);
+       setHasPermission(status === 'granted');
+      
     })();
   }, []);
 
@@ -21,19 +28,67 @@ export default function TakePhoto() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
+  
+  const cameraFolder= async()=>{
+    console.log("camera roll granted")
+    
+      let {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if(status === 'granted'){
+         
+         setCameraRoll(true)
+      }else{
+        
+        setCameraRoll(false)
+      }
+      let result=await ImagePicker.launchImageLibraryAsync({
+        mediaTypes:ImagePicker.MediaTypeOptions.Images
+      });
+     
+    //  testing()
+  }
+  const takingPhoto=async()=>{
+    if(cameraInvo){
+      // let photo=await cameraInvo.takePictureAsync();
+    //   console.log('tpaca');
+    // const { uri } = await cameraInvo.takePictureAsync();
+    // console.log('uri', uri);
+    // createAlbum({uri})
+    
+ 
+    }
+    
+  }
+  const cameraFlip=()=>{
+    setCameraType(cameraType===Camera.Constants.Type.back? 
+      Camera.Constants.Type.front:Camera.Constants.Type.back)
+  }
+//   async function createAlbum({uri}){
+//     console.log("testing111",uri)
+  
+   
+//      const asset = await MediaLibrary.createAssetAsync(uri);
+    
+//     console.log('asset', asset);
+//     MediaLibrary.createAlbumAsync('Expo', asset)
+//       .then(() => {
+//         Alert.alert('Album created!')
+//       })
+//       .catch(error => {
+//         Alert.alert('An Error Occurred!')
+//       });
+//  }
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={styles.Camera} type={cameraType}>
-        <View>
-          <TouchableOpacity>
-            <Ionicons name="md-folder-open" />
+      <Camera style={styles.Camera} type={cameraType} ref={ref=>{setCameraInvo(ref)}}>
+        <View style={styles.CameraScreen}>
+          <TouchableOpacity style={styles.CameraIcons} onPress={cameraFolder} > 
+            <Ionicons name="md-folder-open" size={40}/>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <MaterialIcons name="linked-camera" />
+          <TouchableOpacity style={styles.CameraIcons} onPress={takingPhoto}>
+            <MaterialIcons name="linked-camera" size={40} />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Ionicons name="ios-reverse-camera" />
+          <TouchableOpacity style={styles.CameraIcons} onPress={cameraFlip}>
+            <Ionicons name="ios-reverse-camera" size={40} />
           </TouchableOpacity>
         </View>
       </Camera>
@@ -44,6 +99,18 @@ export default function TakePhoto() {
 const styles = StyleSheet.create({
   Camera: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
   },
+  CameraScreen:{
+    flex:2, 
+    flexDirection:"row",
+    justifyContent:"space-between",
+    margin:30,
+  },
+  CameraIcons:{
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+      backgroundColor: 'transparent',  
+             
+  }
 });
